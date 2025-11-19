be42e566cb0e921a76741df4c794cc5939a5c20d
Md Rokonuzzaman Reza <shohidm@frontiersemi.com>
2025-11-19 15:11:19 +0600

    Measure button disable and Version number cleared

--- WLI/MainFrm.cpp
+++ WLI/MainFrm.cpp
@@ -40,7 +40,7 @@ BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs) {
 	cs.style &= ~(FWS_ADDTOTITLE);
 
 	// Set the window title here!
-	this->SetTitle(L"FSM RAFT 2.1.2402.5");
+	this->SetTitle(L"FSM RAFT");
 
 	// Remove Menu
 	if (cs.hMenu != NULL) {
